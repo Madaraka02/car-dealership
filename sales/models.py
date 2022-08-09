310fb170076388f503f5f3bd2c3e9efb2d5fcfea
Madaraka02 <victormadaraka@gmail.com>
2022-08-10 01:05:48 +0300

    added database setup

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+class Vehicle(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    condition = models.CharField(max_length=100, null=True, blank=True)
+    make = models.CharField(max_length=100, null=True, blank=True)
+    vehicle_type = models.CharField(max_length=100, null=True, blank=True)
+    model = models.CharField(max_length=100, null=True, blank=True)
+    price = models.DecimalField(max_digits=200, decimal_places=2, null=True, blank=True)
+    mileage = models.CharField(max_length=100, null=True, blank=True)
+    engine_size = models.CharField(max_length=100, null=True, blank=True)
+    power = models.CharField(max_length=100, null=True, blank=True)
+    fuel = models.CharField(max_length=100, null=True, blank=True)
+    gearbox = models.CharField(max_length=100, null=True, blank=True)
+    doors = models.CharField(max_length=100, null=True, blank=True)
+    number_of_seats = models.CharField(max_length=100, null=True, blank=True)
+    additional_description = models.TextField(null=True, blank=True)
+    image = models.FileField(upload_to='cars/images', null=True, blank=True)
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url 
+
+
+
+    def __str__(self):
+        return f"{self.name} - {self.make}"
+
+class VehicleImages(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, null=True, blank=False)
+    image = models.FileField(upload_to='cars/images', null=True, blank=True)
+    
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url 
+
+    def __str__(self):
+        return self.vehicle.name     
